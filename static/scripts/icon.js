c7d163ebc002a6eba17e8696a079d96809374b0f
scoutchorton <scoutchorton@yahoo.com>
2021-11-06 14:20:09 -0400

    Progress on icon component

--- static/scripts/icon.js
+++ static/scripts/icon.js
@@ -12,13 +12,28 @@ const LiveStudio = require("../src/LiveStudio/LiveStudio.js");
 Vue.component("icon", {
 	template: "<img v-bind:src=\"__location\" class=\"livestudio-icon\"/>",
 	props: {
-		module: String,
+		category: String,
 		name: String
+	},
+	data: () => {
+		return {
+			location: ""
+		};
 	},
 	computed: {
 		__location: () => {
-			console.log(this);
-			return path.join(this.module || "", this.name + ".svg");
+			console.log(this, this.category, this.name);
+			return path.join("assets", this.category || "", this.name + ".svg");
 		}
 	}
+	/*
+	watch: {
+		category: category => {
+			this.__location = path.join("assets", category || "", this.name + ".svg");
+		},
+		name: name => {
+			this.__location = path.join("assets", this.category || "", name + ".svg");
+		}
+	}
+	*/
 });
