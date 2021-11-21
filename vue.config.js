edbaf6a30ca1a4d0a3da39d17fc2cf6ea1d15c19
scoutchorton <scoutchorton@yahoo.com>
2021-11-21 10:29:28 -0500

    Better vue-cli config

--- vue.config.js
+++ vue.config.js
@@ -2,28 +2,40 @@ module.exports = {
 	pluginOptions: {
 		electronBuilder: {
 			nodeIntegration: true,
-			nodeIntegrationInWorker: true
-		},
-	},
-	configureWebpack: [
-		/*
-		entry: {
-			frontend: {
-				import: "./src/main.ts",
-				library: {
-					name: "electron",
-					type: "umd"
-				}
+			nodeIntegrationInWorker: true,
+			chainWebpackMainProcess: (config) => {
+				//console.log(config);
+				//console.dir(config);
+				config.module
+					.rule("vue")
+					.test(/\.vue$/)
+					.use("vue-loader")
+						.loader("vue-loader");
 			},
-			backend: {
-				import: "./api/LiveStudio.ts",
-				library: {
-					name: "livestudio",
-					type: "commonjs"
-				}
+			mainProcessFile: "./src/main.ts",
+			renderProcessFile: "./src/background.ts"
+		}
+	},
+	/*
+	entry: {
+		frontend: {
+			import: "./src/main.ts",
+			library: {
+				name: "electron",
+				type: "umd"
+			}
+		},
+		backend: {
+			import: "./api/LiveStudio.ts",
+			library: {
+				name: "livestudio",
+				type: "commonjs"
 			}
 		}
-		*/
+	}
+	*/
+	/*
+	configureWebpack: [
 		{
 			target: "node",
 			entry: "./api/LiveStudio.ts"
@@ -33,5 +45,27 @@ module.exports = {
 			entry: "./src/main.ts"
 		}
 	],
+	*/
+	/*
+	configureWebpack: () => {
+		return [
+			{
+				entry: "./api/LiveStudio.ts",
+				target: "electron-main",
+				name: "api"
+			},
+			{
+				entry: "./src/main.ts",
+				target: "electron-renderer",
+				name: "electron"
+			},
+		]
+		return {
+			entry: "./src/main.ts",
+			target: "electron-renderer",
+			name: "electron"
+		}
+	},
+	*/
 	lintOnSave: false
 }
