681569d6e5c1147cd7b00fb7c4fcd15e312694e7
scoutchorton <scoutchorton@yahoo.com>
2021-12-26 10:37:43 -0500

    Started to implement statusbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { StatusbarComponent } from './statusbar/statusbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+  		StatusbarComponent
+	],
+	imports: [
+		BrowserModule
+	],
+	providers: [],
+	bootstrap: []
 })
-export class AppModule { }
+export class AppModule {}
